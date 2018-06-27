41c43491ebe65446348fcacfa1941887b0f1b0ab
Isabel Greiner <ivg2@fb.com>
2018-06-27 14:58:49 -0700

    Initial commit - finished todo list application

---
+++ app/src/main/java/me/ivg2/todoapplication/MainActivity.java
@@ -0,0 +1,92 @@
+package me.ivg2.todoapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        //items.add("ToDo Item #1");
+        //items.add("ToDo Item #2");
+
+        //incorporates the method which allows user to delete a task
+        setupListViewListener();
+    }
+
+    public void onAddItem(View view) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+
+        writeItems();
+
+        Toast.makeText(getApplicationContext(), "Item added to list!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long x) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+
+                writeItems();
+
+                //shows a message to user after removing an item
+                Log.i("MainActivity", "Removed item " + position);
+                return true;
+            }
+
+        });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems() {
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
